9f65af93084bdd81800f128f4f2989a2ba7159ef
Anas Maroof <anas-maroof@users.noreply.github.com>
2026-01-07 17:58:35 +0530

    Login Page created and API call sucessfull

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,11 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
 
-export default Login
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+
+          {/* Email */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-sm">Email ID</legend>
+            <input
+              type="text"
+              value={emailId}
+              className="input input-bordered w-full"
+              placeholder="email@xyz.com"
+              required
+              onChange={(e) => setEmailId(e.target.value)}
+            />
+          </fieldset>
+
+          {/* Password */}
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend text-sm">Password</legend>
+            <input
+              type="password"
+              value={password}
+              className="input input-bordered w-full"
+              placeholder="Password"
+              required
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+
+          <div className="card-actions justify-center mt-4">
+            <button className="btn btn-primary w-full" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
