86904bf16123243c954b3e2d048c0e7a13c27403
Anas Maroof <anas-maroof@users.noreply.github.com>
2026-01-08 14:43:17 +0530

    Add authorization to other routes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,18 +1,43 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constant";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
+
+  const fetchUser = async () => {
+    try {
+      const user = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(user.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      } else {
+        console.log(err.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (!userData) {
+      fetchUser();
+    }
+  }, []);
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-
-      {/* Main content */}
       <main className="grow">
         <Outlet />
       </main>
-
       <Footer />
     </div>
   );
