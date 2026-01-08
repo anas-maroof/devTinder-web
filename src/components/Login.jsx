bf9fcdfb3233eb1d3f58372c05e8d5a9a5047967
Anas Maroof <anas-maroof@users.noreply.github.com>
2026-01-08 14:05:00 +0530

    Intialize redux-store and add loggedin-user into it

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,28 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constant";
 
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        "http://localhost:7777/login",
+        BASE_URL + "/login",
         {
           emailId,
           password,
         },
         { withCredentials: true }
       );
+      dispatch(addUser(res.data));
+      navigate("/");
     } catch (err) {
       console.error(err);
     }
